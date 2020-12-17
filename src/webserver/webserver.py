942606803053cb9888aeec56266123489583f955
Aapo Keskimölö <aapo@synopsys.com>
2020-12-17 16:17:54 +0200

    Change flask to simple http fileserver
    The simple file server allows serving files over http requests with
    less code than flask. The html content is provided over requests
    with any path following url '/'.
    
    The directory is not available with python 3.5.3 so the current working
    directory will be used to resolve the working directory of the http
    server. The folders are added to gitignore to avoid seeing them as
    untracked files.

--- src/webserver/webserver.py
+++ src/webserver/webserver.py
@@ -1,37 +1,93 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from http.server import HTTPServer, SimpleHTTPRequestHandler, HTTPStatus
 import sys
 from argparse import ArgumentParser
+import json
+import time
+import logging
+import os
+from functools import partial
 
 
-app = Flask(__name__)
+__version__ = "0.0"
 
 
-@app.route('/')
-def index():
-    return "You are running python {0}".format(sys.version)
+WEBSERVER_WORKING_DIR = os.environ.get("WEBSERVER_WORKING_DIR", os.path.join(os.getcwd(), "data"))
+os.makedirs(WEBSERVER_WORKING_DIR, exist_ok=True)
+WEBSERVER_LOG = os.environ.get("WEBSERVER_LOG", os.path.join(
+    WEBSERVER_WORKING_DIR, "webserver.log"))
 
+log_fmt = "%(asctime)s %(levelname)s %(filename)s:%(lineno)d: %(message)s"
+logging.basicConfig(
+    filename=WEBSERVER_LOG,
+    level=logging.INFO,
+    format=log_fmt
+)
+log = logging.getLogger(__name__)
+sh = logging.StreamHandler()
+sh.setFormatter(logging.Formatter(log_fmt))
+log.addHandler(sh)
 
-@app.route('/api/post', methods=["POST"])
-def post():
-    supported_ctypes = ["application/json"]
-    if request.content_type not in supported_ctypes:
-        return "Unsupported content-type: {0}, supported types: {1}".format(request.content_type, supported_ctypes)
-    return "{0} - POST received with data: {1}".format(request.host, request.get_json())
+class RequestHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
+    def handle_error(self, status, message):
+        log.error("HTTP %d: %s", status, message)
+        self.send_error(HTTPStatus.NOT_IMPLEMENTED, message)
 
-def run_forever(args):
-    app.run(debug=args.debug, port=args.port, host="0.0.0.0")
+    def do_POST(self):
+        ctype = self.headers.get("content-type")
+        if ctype != "application/json":
+            self.handle_error(HTTPStatus.NOT_IMPLEMENTED,
+                              "unsupported content-type: {0}".format(ctype))
+            return
+
+        if self.headers.get("content-length", 0) == 0:
+            self.handle_error(HTTPStatus.NO_CONTENT, "body is empty")
+            return
+
+        lbuf = 1024
+        left = int(self.headers["Content-Length"])
+        while left > 0:
+            if lbuf > left:
+                lbuf = left
+            data = self.rfile.read(lbuf)
+            left -= len(data)
+
+        path = os.path.join(WEBSERVER_WORKING_DIR, time.strftime("%s"))
+        try:
+            with open(path, "w") as fd:
+                json.dump(data.decode(), fd)
+            log.info("POST saved: %s", path)
+        except Exception as e:
+            log.exception("error")
+            self.handle_error(HTTPStatus.INTERNAL_SERVER_ERROR,
+                              "saving data to server failed: {0}".format(e))
+
+        self.send_response(
+            HTTPStatus.OK, "data saved on server: {0}".format(path))
+        self.flush_headers()
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(
         prog="Webserver CLI for serving remote temperature and humidity controller data.")
     parser.add_argument(
-        "--port", default=3838, type=int, help="Port to listen on")
+        "--port", default=3838, type=int, help="Port to listen on.")
     parser.add_argument(
-        "--debug", action="store_true", help="Enable debugging")
+        "--working-dir", action="store_true", default="./data", help="Directory for serving results."
+    )
     args = parser.parse_args(sys.argv[1:])
 
-    run_forever(args)
+    address = ("", args.port)
+    server = HTTPServer(address, partial(
+        RequestHandler))
+    host, port = server.server_address
+    log.info(
+        "Server running on http://%s:%d at %s", host, port, args.working_dir)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
