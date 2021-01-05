d779f912026ec790ea1d92b3c73ab2d46762184d
Aapo Keskimölö <aapo@synopsys.com>
2021-01-05 12:30:55 +0200

    Pass default port from docker file
    Expose the docker port on the environment when using
    publish all ports -P when running the docker container.
    
    This enables running the HTTP server in container
    
     docker run -p 80:3838 akeskimo/webserver:latest
    
    and accessing it in browser with http://localhost.

--- docker/webserver/entrypoint.sh
+++ docker/webserver/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-cd srv/webserver
-./webserver.py --port 3838
+set -o errexit
+set -o nounset
+set -o pipefail
+
+WEBSERVER_PORT=${WEBSERVER_PORT-}
+
+if [[ -z "$WEBSERVER_PORT" ]]; then
+ echo "no webserver port set in environment"
+ exit 1
+fi
+
+./webserver.py --port $WEBSERVER_PORT
