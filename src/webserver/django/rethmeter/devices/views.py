e363a66508f457bc15ae7f900ef640e67698a8bf
Aapo Keskimolo <aapokesk@gmail.com>
2021-11-03 05:55:53 +0200

    Add base html and index pages

--- src/webserver/django/rethmeter/devices/views.py
+++ src/webserver/django/rethmeter/devices/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from .models import Device, Sensor, Measurement
 
 
 def index(request):
     context = {}
-    return render(request, "home.html", context=context)
+
+    devices = Device.objects.all()
+
+    context['devices'] = [
+        {
+            'uuid': d.id,
+            'name': d.name,
+            'sensors': []
+        } for d in devices or []
+    ]
+
+    for device in context['devices'] or []:
+        sensors = Sensor.objects.filter(parent__exact=device['uuid'])
+
+        device['sensors'] = [
+            {
+                'uuid': m.id,
+                'num_measurements': Measurement.objects.filter(sensor__exact=m.id).count()
+            } for m in sensors
+        ]
+
+    return render(request, 'index.html', context=context)
